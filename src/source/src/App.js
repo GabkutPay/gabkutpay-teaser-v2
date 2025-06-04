251e718a7c52c480f1c7d9069d42ebdeffb8b92e
GabkutPay <kutalagael@gmail.com>
2025-06-05 00:12:58 +0100

    App.js

---
+++ src/source/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur Gabkut Pay Teaser</h1>
+      <p>La version finale arrive bientôt.</p>
+    </div>
+  );
+}
+
+export default App;
